aee9f0c3989a3f8ac322a595a098735bc62da870
avital gutman <avital6297@gmail.com>
2023-07-08 19:49:45 +0300

    add ghosts

--- PACMAN/code.js
+++ PACMAN/code.js
@@ -1,7 +1,10 @@
 var context = canvas.getContext("2d");
 var shape = new Object();
+let ghosts = new Array();
 var board;
+const stack = ["yellow", "blue", "pink", "red"];
 var score;
+let life = 5;
 var pac_color;
 var start_time;
 var time_elapsed;
@@ -18,6 +21,7 @@ function Start() {
 	var cnt = 100;
 	var food_remain = 50;
 	var pacman_remain = 1;
+	let ghost_remain = 4;
 	start_time = new Date();
 	for (var i = 0; i < 10; i++) {
 		board[i] = new Array();
@@ -25,7 +29,21 @@ function Start() {
 		for (var j = 0; j < 10; j++) {
 			if ((i === 3 && j === 3) || (i === 3 && j === 4) || (i === 3 && j === 5) || (i === 6 && j === 1) || (i === 6 && j === 2)) {
 				board[i][j] = 4;
-			} else {
+			}
+			else if ((i === 0 && j === 0) || (i === 0 && j === 9) || (i === 9 && j === 0) || (i === 9 && j === 9)) {
+				if (ghost_remain > 0) {
+					let ghost = new Object();
+					ghost.i = i;
+					ghost.j = j;
+					ghost.k = 0;
+					board[i][j] = 5;
+					ghosts.push(ghost);
+					ghost_remain--;
+				} else {
+					board[i][j] = 0;
+				}
+
+			} else if (board[i][j] != 5) {
 				var randomNum = Math.random();
 				if (randomNum <= 1.0 * food_remain / cnt) {
 					food_remain--;
@@ -35,7 +53,9 @@ function Start() {
 					shape.j = j;
 					pacman_remain--;
 					board[i][j] = 2;
-				} else {
+
+				}
+				else {
 					board[i][j] = 0;
 				}
 				cnt--;
@@ -88,7 +108,7 @@ function GetKeyPressed() {
 	}
 }
 
-function Draw(direction) {
+function Draw(direction, newGhostPosition) {
 	context.clearRect(0, 0, canvas.width, canvas.height); //clean board
 	lblScore.value = score;
 	lblTime.value = time_elapsed;
@@ -150,7 +170,12 @@ function Draw(direction) {
 			} else if (board[i][j] === 4) {
 				context.beginPath();
 				context.rect(center.x - 30, center.y - 30, 60, 60);
-				context.fillStyle = "grey"; //color
+				context.fillStyle = "grey"; //color     
+				context.fill();
+			} else if (board[i][j] === 5) {
+				context.beginPath();
+				context.arc(center.x, center.y, 15, 0, 2 * Math.PI); // circle
+				context.fillStyle = "pink"; //color
 				context.fill();
 			}
 		}
@@ -183,8 +208,19 @@ function UpdatePosition() {
 	}
 	if (board[shape.i][shape.j] === 1) {
 		score++;
+
+	}
+	if (board[shape.i][shape.j] === 5) {
+		score--;
+		life--;
 	}
 	board[shape.i][shape.j] = 2;
+	let ghost = ghosts[0];
+	let newGhostPosition = ghostAlgorithm(ghost, shape);
+	board[ghost.i][ghost.j] = ghost.k;
+	newGhostPosition.k = board[newGhostPosition.i][newGhostPosition.j];
+	ghosts.splice(0, 0, newGhostPosition);
+	board[newGhostPosition.i][newGhostPosition.j]= 5;
 	var currentTime = new Date();
 	time_elapsed = (currentTime - start_time) / 1000;
 	if (score >= 20 && time_elapsed <= 10) {
@@ -196,4 +232,127 @@ function UpdatePosition() {
 	} else {
 		Draw(last_pressed_key);
 	}
+}
+
+function ghostAlgorithm(ghost, pacman) {
+	let newGhostPosition = new Object();
+	let directionsMap = new Map();
+	directionsMap.set("Up", 1);
+	directionsMap.set("Down", 1);
+	directionsMap.set("Left", 1);
+	directionsMap.set("Right", 1);
+
+	if (ghost.i + 1 > 9) {
+		directionsMap.set("Right", 0);
+	}
+	if (ghost.i - 1 < 0) {
+		directionsMap.set("Left", 0);
+	}
+	if (ghost.j + 1 > 9) {
+		directionsMap.set("Down", 0);
+	}
+	if (ghost.j - 1 < 0) {
+		directionsMap.set("Up", 0);
+	}
+
+	if (ghost.i === pacman.i) {
+		if (ghost.j < pacman.j) {
+			if (board[ghost.i][ghost.j + 1] != 4) {
+				newGhostPosition.i = ghost.i;
+				newGhostPosition.j = ghost.j + 1;
+			} else {
+				directionsMap.set("Down", 0);
+				newGhostPosition = findNewPositionForGhost(directionsMap, newGhostPosition, ghost);
+			}
+		}
+		else if (ghost.j > pacman.j) {
+			if (board[ghost.i][ghost.j - 1] != 4) {
+				newGhostPosition.i = ghost.i;
+				newGhostPosition.j = ghost.j - 1;
+			} else {
+				directionsMap.set("Up", 0)
+				newGhostPosition = findNewPositionForGhost(directionsMap, newGhostPosition, ghost);
+			}
+		}
+	}
+	else if (ghost.j === pacman.j) {
+		if (ghost.i < pacman.i) {
+			if (board[ghost.i + 1][ghost.j] != 4) {
+				newGhostPosition.i = ghost.i + 1;
+				newGhostPosition.j = ghost.j;
+			}
+			else {
+				directionsMap.set("Right", 0);
+				newGhostPosition = findNewPositionForGhost(directionsMap, newGhostPosition, ghost);
+			}
+		}
+		else if (ghost.i > pacman.i) {
+			if (board[ghost.i - 1][ghost.j] != 4) {
+				newGhostPosition.i = ghost.i - 1;
+				newGhostPosition.j = ghost.j;
+			} else {
+				directionsMap.set("Left", 0);
+				newGhostPosition = findNewPositionForGhost(directionsMap, newGhostPosition, ghost);
+			}
+		}
+	}
+	else {
+		newGhostPosition = findNewPositionForGhost(directionsMap, newGhostPosition, ghost);
+	}
+
+	return newGhostPosition;
+}
+
+function findNewPositionForGhost(directionsMap, newGhostPosition, ghost) {
+	let newPositionIsSet = false;
+	while (!newPositionIsSet) {
+		let randomDirection = randomNumber(1, 4);
+		let isValidDirection = isValidGhostDirection(directionsMap, randomDirection)
+		if (isValidDirection === 1) {
+			newGhostPosition = detectNewGhostPosition(randomDirection, newGhostPosition, ghost)
+			newPositionIsSet = true;
+		}
+	}
+	return newGhostPosition;
+}
+
+function randomNumber(min, max) {
+	return Math.floor(Math.random() * (max - min) + min);
+}
+
+function isValidGhostDirection(directionsMap, direction) {
+	let isValid
+	if (direction === 1) {
+		isValid = directionsMap.get("Up");
+	}
+	if (direction === 2) {
+		isValid = directionsMap.get("Down");
+	}
+	if (direction === 3) {
+		isValid = directionsMap.get("Left");
+	}
+	if (direction === 4) {
+		isValid = directionsMap.get("Right");
+	}
+	return isValid
+}
+
+function detectNewGhostPosition(direction, newGhostPosition, ghost) {
+	if (direction === 1) {
+		newGhostPosition.i = ghost.i;
+		newGhostPosition.j = ghost.j - 1;
+	}
+	if (direction === 2) {
+		newGhostPosition.i = ghost.i;
+		newGhostPosition.j = ghost.j + 1;
+	}
+	if (direction === 3) {
+		newGhostPosition.i = ghost.i - 1;
+		newGhostPosition.j = ghost.j;
+	}
+	if (direction === 4) {
+		newGhostPosition.i = ghost.i + 1;
+		newGhostPosition.j = ghost.j;
+	}
+	return newGhostPosition;
 }
